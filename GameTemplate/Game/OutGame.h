8adec6f374d1d2ab3f0301f3a4d403bde3e8b6d2
takeda haruse <haruse010655@gmail.com>
2025-03-03 14:14:20 +0900

    クラス作成まで

---
+++ GameTemplate/Game/OutGame.h
@@ -0,0 +1,14 @@
+#pragma once
+class OutGame : public IGameObject
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	OutGame() {}
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~OutGame() {}
+};
+
